9d7b54a4b3f83f9d3649accadc3d85c4503bed9e
kaua <112486548+KLZ17@users.noreply.github.com>
2024-11-06 19:47:53 -0300

    06/11

---
+++ graficos/common.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+      x: 'nomeDasRedes',
+      y: quantidadeDeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color')
+      }
+    }
+  ]
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color')
+}
